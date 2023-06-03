2963705f67421b31eb430e31be47741dab6629ed
haseebzeeshan2010 <haseebzeeshan2010@gmail.com>
2023-06-03 20:34:05 +0100

    Adding the Big O notation of each function

--- Efficiency.py
+++ Efficiency.py
@@ -3,20 +3,20 @@ a single manatee has properties like "name", "age", et cetera
 n = the number of elements in "manatees"
 m = the number of properties per "manatee" (i.e. the number of keys in a manatee dictionary)"""
 
-def example1(manatees):
+def example1(manatees): #O(n)
     for manatee in manatees:
         print(manatee['name'])
 
-def example2(manatees):
+def example2(manatees): #O(1)
     print(manatees[0]['name'])
     print(manatees[0]['age'])
 
-def example3(manatees):
+def example3(manatees): #O(mn)
     for manatee in manatees:
         for manatee_property in manatee:
             print(manatee_property, ": ", manatee[manatee_property])
 
-def example4(manatees):
+def example4(manatees): #O(n^2)
     oldest_manatee = "No manatees here!"
     for manatee1 in manatees:
         for manatee2 in manatees:
