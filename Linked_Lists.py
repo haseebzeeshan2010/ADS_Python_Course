f6baf0e97ac4dc3b479720a226d6d2c9c28da1c5
haseebzeeshan2010 <haseebzeeshan2010@gmail.com>
2023-06-07 22:54:13 +0100

    Created a linked list

---
+++ Linked_Lists.py
@@ -0,0 +1,49 @@
+class Element(object):
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+        
+class LinkedList(object):
+    def __init__(self, head=None):
+        self.head = head
+        
+    def append(self, new_element):
+        current = self.head
+        if self.head:
+            while current.next:
+                current = current.next
+            current.next = new_element
+        else:
+            self.head = new_element
+            
+    def get_position(self, position):
+        """Get an element from a particular position.
+        Assume the first position is "1".
+        Return "None" if position is not in the list."""
+        counter = 1
+        current = self.head
+        while current:
+            if counter == position:
+                return(current)
+            current = current.next
+            counter += 1
+        return None
+
+# Test cases
+# Set up some Elements
+e1 = Element(1)
+e2 = Element(2)
+e3 = Element(3)
+e4 = Element(4)
+
+# Start setting up a LinkedList
+ll = LinkedList(e1)
+ll.append(e2)
+ll.append(e3)
+
+
+# Test get_position
+# Should print 3
+print (ll.head.next.next.value)
+# Should also print 3
+print (ll.get_position(3).value)
