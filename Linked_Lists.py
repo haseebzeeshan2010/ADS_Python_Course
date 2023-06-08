cbe0edabfeac07fd528e581820e6ab144557b2d9
haseebzeeshan2010 <haseebzeeshan2010@gmail.com>
2023-06-08 15:28:03 +0100

    Function to insert items and delete first item

--- Linked_Lists.py
+++ Linked_Lists.py
@@ -1,3 +1,5 @@
+
+
 class Element(object):
     def __init__(self, value):
         self.value = value
@@ -28,6 +30,51 @@ class LinkedList(object):
             current = current.next
             counter += 1
         return None
+    
+    def insert(self, new_element, position):
+        """Insert a new node at the given position.
+        Assume the first position is "1".
+        Inserting at position 3 means between
+        the 2nd and 3rd elements."""
+        counter = 1
+        current = self.head
+        while counter != position-1:
+            current = current.next
+            counter += 1
+        #current.next = current
+        
+        # current = new_element
+        
+        temp = current.next
+        current.next = new_element
+        current.next.next = temp
+        
+        
+        return None
+        
+        #pass
+    
+    
+    def delete(self, value):
+        """Delete the first node with a given value."""
+        current = self.head
+        while current.value != value:
+            current = current.next
+        
+        
+        
+        self.head = self.head.next
+        
+        
+        # current = new_element
+        
+        
+        #current.next = new_element
+        #current.next.next = temp
+        
+        
+        return None
+        pass
 
 # Test cases
 # Set up some Elements
@@ -46,4 +93,18 @@ ll.append(e3)
 # Should print 3
 print (ll.head.next.next.value)
 # Should also print 3
+print (ll.get_position(3).value)
+
+# Test insert
+ll.insert(e4,3)
+# Should print 4 now
+print (ll.get_position(3).value)
+
+# Test delete
+ll.delete(1)
+# Should print 2 now
+print (ll.get_position(1).value)
+# Should print 4 now
+print (ll.get_position(2).value)
+# Should print 3 now
 print (ll.get_position(3).value)
