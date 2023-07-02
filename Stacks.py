e52ded7ac0c345f3999fd54ce6c0f3103f08d468
haseebzeeshan2010 <haseebzeeshan2010@gmail.com>
2023-07-02 20:36:47 +0100

    Debugged and completed delete function

--- Stacks.py
+++ Stacks.py
@@ -1,20 +1,12 @@
-"""Add a couple methods to our LinkedList class,
-and use that to implement a Stack.
-You have 4 functions below to fill in:
-insert_first, delete_first, push, and pop.
-Think about this while you're implementing:
-why is it easier to add an "insert_first"
-function than just use "append"?"""
-
 class Element(object):
     def __init__(self, value):
         self.value = value
         self.next = None
-        
+
 class LinkedList(object):
     def __init__(self, head=None):
         self.head = head
-        
+
     def append(self, new_element):
         current = self.head
         if self.head:
@@ -26,35 +18,31 @@ class LinkedList(object):
 
     def insert_first(self, new_element):
         "Insert new element as the head of the LinkedList"
-        self.head
-        self.head.next
+        new_element.next = self.head
+        self.head = new_element
 
     def delete_first(self):
-        h_temp = self.head
-        if h_temp :
-            self.head = h_temp.next
-        return h_temp
+        "Delete the first (head) element in the LinkedList as return it"
+        if self.head:
+            temp = self.head
+            self.head = self.head.next
+            temp.next = None
+            return temp
+        else:
+            return None
 
 class Stack(object):
-    def __init__(self,top=None):
+    def __init__(self, top=None):
         self.ll = LinkedList(top)
 
     def push(self, new_element):
         "Push (add) a new element onto the top of the stack"
-        current = self.head
-        if self.head:
-            while current.next:
-                current = current.next
-            current.next = new_element
-        else:
-            self.head = new_element
-        pass
+        self.ll.insert_first(new_element)
 
     def pop(self):
         "Pop (remove) the first element off the top of the stack and return it"
-        
-        pass
-    
+        return self.ll.delete_first()
+
 # Test cases
 # Set up some Elements
 e1 = Element(1)
@@ -68,9 +56,9 @@ stack = Stack(e1)
 # Test stack functionality
 stack.push(e2)
 stack.push(e3)
-print stack.pop().value
-print stack.pop().value
-print stack.pop().value
-print stack.pop()
+print(stack.pop().value)  # Output: 3
+print(stack.pop().value)  # Output: 2
+print(stack.pop().value)  # Output: 1
+print(stack.pop())        # Output: None
 stack.push(e4)
-print stack.pop().value
+print(stack.pop().value)  # Output: 4
