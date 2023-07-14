7646304b66d90e996ac15e0d5e19da5703c95dca
haseebzeeshan2010 <haseebzeeshan2010@gmail.com>
2023-07-14 21:10:31 +0100

    Created for l

---
+++ Quick_sort.py
@@ -0,0 +1,12 @@
+import random
+
+num_numbers = 1000
+min_value = 1
+max_value = 1000
+random_numbers = []
+
+for _ in range(num_numbers):
+    random_number = random.randint(min_value, max_value)
+    random_numbers.append(random_number)
+
+print(random_numbers)
