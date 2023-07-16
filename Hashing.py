75490fc8526cc3d8c8406852e75e68f14afe4e09
haseebzeeshan2010 <haseebzeeshan2010@gmail.com>
2023-07-16 21:30:40 +0100

    Added key value pairs to hash map

--- Hashing.py
+++ Hashing.py
@@ -1,2 +1,8 @@
-num = int(input("Enter code: ")[-2:])
-print(num%12)
+# Create an empty hash map
+hash_map = {}
+
+# Add key-value pairs to the hash map
+hash_map["apple"] = 1
+hash_map["banana"] = 2
+hash_map["cherry"] = 3
+
