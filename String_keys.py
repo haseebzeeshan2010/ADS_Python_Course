b1f10c99c6d9a9c00bd14b017dcfe40394ab322d
haseebzeeshan2010 <haseebzeeshan2010@gmail.com>
2023-07-16 22:20:14 +0100

    Completed lookup and calculate_hash_value function

--- String_keys.py
+++ String_keys.py
@@ -20,12 +20,16 @@ class HashTable(object):
         """Return the hash value if the
         string is already in the table.
         Return -1 otherwise."""
+        hash_value = self.calculate_hash_value(string)
+        if self.table[hash_value] is not None:
+            if string in self.table[hash_value]:
+                return hash_value
         return -1
 
     def calculate_hash_value(self, string):
         """Helper function to calulate a
         hash value from a string."""
-        return -1
+        return (ord(string[0]) * 100) + ord(string[1])
     
 # Setup
 hash_table = HashTable()
@@ -41,9 +45,9 @@ print hash_table.lookup('UDACITY')
 # Test store
 hash_table.store('UDACITY')
 # Should be 8568
-print hash_table.lookup('UDACITY')
+print(hash_table.lookup('UDACITY'))
 
 # Test store edge case
 hash_table.store('UDACIOUS')
 # Should be 8568
-print hash_table.lookup('UDACIOUS')
+print(hash_table.lookup('UDACIOUS'))
