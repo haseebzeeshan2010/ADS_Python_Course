588ad5ea0492148a91217ebf86443c3cfddf62df
haseebzeeshan2010 <haseebzeeshan2010@gmail.com>
2023-08-13 19:58:53 +0100

    Added the graph

--- Djikstra.py
+++ Djikstra.py
@@ -20,3 +20,14 @@ def dijkstra(graph, start):
                 heapq.heappush(queue, (distance, neighbor))
 
     return distances
+
+
+graph = {
+    'A': {'B': 4, 'C': 2},
+    'B': {'A': 4, 'C': 1, 'D': 5},
+    'C': {'A': 2, 'B': 1, 'D': 8},
+    'D': {'B': 5, 'C': 8}
+}
+
+shortest_distances = dijkstra(graph, 'A')
+print(shortest_distances)
