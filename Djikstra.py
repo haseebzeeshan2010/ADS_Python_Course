1cc228461b4887141de4d0406a6009e40bbcc18f
haseebzeeshan2010 <haseebzeeshan2010@gmail.com>
2023-08-13 19:59:14 +0100

    Made the algorithm generate 200 nodes

--- Djikstra.py
+++ Djikstra.py
@@ -1,4 +1,5 @@
 import heapq
+import random
 
 def dijkstra(graph, start):
     distances = {node: float('inf') for node in graph}
@@ -21,13 +22,29 @@ def dijkstra(graph, start):
 
     return distances
 
+# Create an empty graph
+graph = {}
 
-graph = {
-    'A': {'B': 4, 'C': 2},
-    'B': {'A': 4, 'C': 1, 'D': 5},
-    'C': {'A': 2, 'B': 1, 'D': 8},
-    'D': {'B': 5, 'C': 8}
-}
 
-shortest_distances = dijkstra(graph, 'A')
+# Add 200 nodes to the graph
+for i in range(200):
+    node = str(i)
+    graph[node] = {}
+
+# Add random edges between the nodes
+for i in range(200):
+    for j in range(i+1, 200):
+        node1 = str(i)
+        node2 = str(j)
+        weight = random.randint(1, 10)  # Random weight between 1 and 10
+        graph[node1][node2] = weight
+        graph[node2][node1] = weight
+
+print(graph)
+
+start_node = list(graph.keys())[0]
+
+# Calculate the shortest distances from the start node
+shortest_distances = dijkstra(graph, start_node)
+
 print(shortest_distances)
