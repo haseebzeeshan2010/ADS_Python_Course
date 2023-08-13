380e9cd51b98994cad3522e99cb08cbeaabe7ec2
haseebzeeshan2010 <haseebzeeshan2010@gmail.com>
2023-08-13 20:04:44 +0100

    Imported original dijkstra algorithm

---
+++ Dijkstra_efficient.py
@@ -0,0 +1,53 @@
+import heapq
+import random
+
+def dijkstra(graph, start):
+    distances = {node: float('inf') for node in graph}
+    distances[start] = 0
+
+    queue = [(0, start)]
+
+    while queue:
+        current_distance, current_node = heapq.heappop(queue)
+
+        if current_distance > distances[current_node]:
+            continue
+
+        for neighbor, weight in graph[current_node].items():
+            distance = current_distance + weight
+
+            if distance < distances[neighbor]:
+                distances[neighbor] = distance
+                heapq.heappush(queue, (distance, neighbor))
+
+    return distances
+
+# Create an empty graph
+graph = {}
+
+
+#Create a provided number of nodes
+NODES = 10000
+
+# Add 200 nodes to the graph
+for i in range(NODES):
+    node = str(i)
+    graph[node] = {}
+
+# Add random edges between the nodes
+for i in range(NODES):
+    for j in range(i+1, NODES):
+        node1 = str(i)
+        node2 = str(j)
+        weight = random.randint(1, 10)  # Random weight between 1 and 10
+        graph[node1][node2] = weight
+        graph[node2][node1] = weight
+
+print("Generated Graph")
+
+start_node = list(graph.keys())[0]
+
+# Calculate the shortest distances from the start node
+shortest_distances = dijkstra(graph, start_node)
+
+print(shortest_distances)
